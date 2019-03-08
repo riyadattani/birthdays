bdf3925c34aaa65df510c26d4f4015c0371b74a5
Riya Dattani <riya_dattani@hotmail.com>
2019-03-08 11:58:54 +0000

    can add to and show the birthday list

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,21 @@
+class BirthdayList
 
+  def initialize
+    @birthday_list = []
+  end
+
+  def add_birthday(name, birthday)
+    @birthday_list << { name: name, birthday: birthday }
+  end
+
+  def show_birthdays
+    @birthday_list.each do |hash|
+      puts "#{hash[:name]}: #{hash[:birthday]}"
+    end
+  end
+
+end
+
+# birthday_list = BirthdayList.new
+# birthday_list.add_birthday("Riya", "14 Jul 95")
+# p birthday_list
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,25 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  it 'add a name and birthday' do
+    birthday_list = BirthdayList.new
+
+    expect(birthday_list.add_birthday("Riya", "14 Jul 95")).to eq [{ name: "Riya", birthday: "14 Jul 95" }]
+  end
+
+  it 'add a second birthday' do
+    birthday_list = BirthdayList.new
+    birthday_list.add_birthday("Riya", "14 Jul 95")
+
+    expect(birthday_list.add_birthday("Mum", "7 Mar 68")).to eq [{ name: "Riya", birthday: "14 Jul 95" }, { name: "Mum", birthday: "7 Mar 68" }]
+  end
+
+  it 'show the birthday list neatly' do
+    birthday_list = BirthdayList.new
+    birthday_list.add_birthday("Riya", "14 Jul 95")
+    birthday_list.add_birthday("Mum", "7 Mar 68")
+
+    expect { birthday_list.show_birthdays }.to output("Riya: 14 Jul 95\nMum: 7 Mar 68\n").to_stdout
+  end
+
+end
